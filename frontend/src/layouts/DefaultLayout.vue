68a02419e7319c38173a40796d475b5005afa809
Vic1708 <vi.silvaf@duocuc.cl>
2026-05-30 01:53:15 -0400

    fix: eliminar link Admin duplicado (ya esta en el interruptor)

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -60,9 +60,6 @@
                   <span v-if="cartCount > 0" class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">{{ cartCount }}</span>
                 </router-link>
               </template>
-
-              <!-- Admin quick link always visible if admin -->
-              <router-link v-if="isAdmin && modoActual !== 'admin'" to="/admin" class="nav-link text-primary text-xs">Admin</router-link>
             </template>
           </div>
 

ad2a591e8312dff74345a8b07831ebc6174e9095
Darlette <darlettemorales@gmail.com>
2026-06-19 22:42:28 -0400

    fix: centrar modal de confirmacion en pantalla
    Co-Authored-By: Darlette <darlettemorales@gmail.com>

--- frontend/src/components/BottomSheet.vue
+++ frontend/src/components/BottomSheet.vue
@@ -1,11 +1,8 @@
 <template>
   <Teleport to="body">
-    <div v-if="modelValue" class="fixed inset-0 bg-black/60 flex items-end justify-center z-50" @click.self="$emit('update:modelValue', false)">
-      <div class="bg-[var(--bg-elevated)] rounded-t-2xl border-t border-x border-white/10 w-full max-w-lg">
-        <div class="w-10 h-1 bg-white/20 rounded-full mx-auto mt-3 mb-1"></div>
-        <div class="p-6">
-          <slot />
-        </div>
+    <div v-if="modelValue" class="fixed inset-0 bg-black/60 flex items-center justify-center z-50 px-4" @click.self="$emit('update:modelValue', false)">
+      <div class="bg-[var(--bg-elevated)] rounded-2xl border border-white/10 w-full max-w-lg p-6">
+        <slot />
       </div>
     </div>
   </Teleport>
